f7a277365a12e4102884016a424f6b692b8505d0
Enzo Bossé <117042107+Bl0bli@users.noreply.github.com>
2024-03-05 14:35:55 +0100

    .

---
+++ Assets/_PROJECT/Scripts/PathFinding/PathFinding.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinding : MonoBehaviour
+{
+    GridManager gm;
+    List<Vector3> GHF = new List<Vector3>();
+    List<Tile> tilesChecked = new List<Tile>();
+    Piece queen;
+    private void Awake()
+    {
+        gm = FindObjectOfType<GridManager>();
+        queen = GetComponent<Piece>();
+    }
+
+    public void Path()
+    {
+        Tile start = gm.GetTileAt(transform.position);
+
+    }
+}
