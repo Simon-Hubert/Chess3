f32cc428194582f80e1967f23f6825597183557e
Enzo Bossé <117042107+Bl0bli@users.noreply.github.com>
2024-03-08 12:04:33 +0100

    [ADD] Final Animation Teleporter

--- Assets/_PROJECT/Scripts/Features/Teleporter.cs
+++ Assets/_PROJECT/Scripts/Features/Teleporter.cs
@@ -10,8 +10,12 @@ public class Teleporter : MonoBehaviour
     [SerializeField] AnimationCurve speed = AnimationCurve.Linear(0f, 0f, 1f, 1f);
     Tile thisTile;
     [SerializeField] UnityEvent onTeleport;
+    [SerializeField] Animator portal;
+    [SerializeField] GameObject Portal;
     private void Awake()
     {
+        Portal = GameObject.Find("Téléporteur");
+        portal = Portal.GetComponent<Animator>();
         thisTile = GetComponent<Tile>();
     }
     private void OnEnable()
@@ -23,23 +27,33 @@ public class Teleporter : MonoBehaviour
         thisTile.onMovedTo -= Teleport;
     }
 
-    public void Teleport(GameObject g)
+    void Teleport(GameObject g)
     {
         StartCoroutine(Teleportation(g));
     }
     IEnumerator Teleportation(GameObject g)
     {
+        bool a = false;
+        bool b = false;
         float elapsedTime = 0f;
         float e2 = 0f;
         Vector2 pos = g.transform.position;
+        Portal.transform.position = new Vector2(transform.position.x, Portal.transform.position.y);
+        portal.SetTrigger("Opening");
         while(elapsedTime < timeLimit)
         {
-            if (pos.y > 8)
+            if (pos.y > 8 && !b)
             {
-                g.transform.position = new Vector2(teleporter.transform.position.x, g.transform.position.x);
+                Debug.Log("if");
+                portal.SetTrigger("Closing");
+                g.transform.position = new Vector2(teleporter.transform.position.x, g.transform.position.y);
+                Portal.transform.position = new Vector2(teleporter.transform.position.x, Portal.transform.position.y);
+                a = true;
+                b = true;
             }
-            if (g.transform.position.x != teleporter.transform.position.x)
+            if (g.transform.position.x != teleporter.transform.position.x && !a)
             {
+                Debug.Log("if2");
                 pos.y = Mathf.Lerp(thisTile.transform.position.y, 8.5f, speed.Evaluate(elapsedTime));
                 g.transform.position = new Vector2(g.transform.position.x, pos.y);
                 elapsedTime += Time.deltaTime;
@@ -47,10 +61,11 @@ public class Teleporter : MonoBehaviour
             }
             else
             {
-                pos.y = Mathf.Lerp(8.5f, teleporter.transform.position.y, speed.Evaluate(e2));
+                Debug.Log("else");
+                pos.y = Mathf.Lerp(g.transform.position.y, teleporter.transform.position.y, speed.Evaluate(e2));
                 g.transform.position = new Vector2(g.transform.position.x, pos.y);
-                elapsedTime += Time.deltaTime;
                 e2 += Time.deltaTime;
+                elapsedTime += Time.deltaTime;
                 yield return null;
             }
 
