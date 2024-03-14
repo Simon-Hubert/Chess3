6c3335cb2022ab589329853f1c8bf1efc33332be
Enzo Bossé <117042107+Bl0bli@users.noreply.github.com>
2024-03-14 12:03:46 +0100

    [ADD] ça mère playstore ça marche en vrai

--- Assets/_PROJECT/Scripts/Success/Authenticate.cs
+++ Assets/_PROJECT/Scripts/Success/Authenticate.cs
@@ -5,54 +5,61 @@ using UnityEngine.SceneManagement;
 using GooglePlayGames;
 using GooglePlayGames.BasicApi;
 using UnityEngine.UIElements;
+using UnityEngine.UI;
 
 public class Authenticate : MonoBehaviour
 {
     [SerializeField] SpriteRenderer square;
+
+    public Text info;
+         
     public static bool Connected { get; private set; }
     private void Start()
     {
         square.color = Color.black;
-        //PlayGamesPlatform.Activate();
+        PlayGamesPlatform.Activate();
         //PlayGamesPlatform.Instance.Authenticate(Connect);
-        if (!Connected)
-            SignIn();
+
+        //if (!Connected)
+        PlayGamesPlatform.Instance.Authenticate(SignIn);
     }
 
-    public void SignIn()
+    public void SignIn(SignInStatus state)
     {
-        if (!PlayGamesPlatform.Instance.IsAuthenticated())
-        {
-            PlayGamesPlatform.Instance.Authenticate((result) =>
-            {
-                if (square.color != null)
-                    square.color = Color.yellow;
-
-                if (result == SignInStatus.Success)
-                {
-                    Connected = true;
-                    square.color = Color.green;
-                }
-            });
-        }
-        else
-        {
-            square.color = Color.red;
-        }
-    }
-    void Connect(SignInStatus state)
-    {
+        //if (!PlayGamesPlatform.Instance.IsAuthenticated())
         square.color = Color.yellow;
-        if(state == SignInStatus.Success)
+        info.text = state.ToString();
+
+        if (state == SignInStatus.Success)
         {
-            Debug.Log("connecté");
+            Connected = true;
             square.color = Color.green;
-            //SceneManager.LoadScene(1);
+            SceneManager.LoadScene(1);
         }
-        else
+        else if (state == SignInStatus.InternalError)
         {
-            PlayGamesPlatform.Instance.ManuallyAuthenticate(Connect);
             square.color = Color.red;
         }
+        else if (state == SignInStatus.Canceled)
+        {
+            square.color = Color.cyan;
+
+            PlayGamesPlatform.Instance.ManuallyAuthenticate(SignIn);
+        }
     }
+    //void Connect(SignInStatus state)
+    //{
+    //    square.color = Color.yellow;
+    //    if(state == SignInStatus.Success)
+    //    {
+    //        Debug.Log("connecté");
+    //        square.color = Color.green;
+    //        //SceneManager.LoadScene(1);
+    //    }
+    //    else
+    //    {
+    //        PlayGamesPlatform.Instance.ManuallyAuthenticate(Connect);
+    //        square.color = Color.red;
+    //    }
+    //}
 }
